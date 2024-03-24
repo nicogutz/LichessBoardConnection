c2e50be0da01bb730e97181e4c58b37c0f0595f6
nicogusuarez <nicolas.gutierrezsuarez@student.kuleuven.be>
2024-03-24 14:45:33 +0100

    Added Header

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -5,6 +5,8 @@ import { MakeMove } from "../components/MakeMove";
 import { StreamGame } from "../components/StreamGame";
 import { getCtrl } from "../lichess/ctrl";
 import { Magnet } from "../utils/Magnet";
+import { Helmet } from "react-helmet";
+import ChassNavbar from "../components/Navbar";
 
 function Play() {
     const ctrl = getCtrl()
@@ -17,11 +19,22 @@ function Play() {
 
     return (
         <>
+            <Helmet>
+                <title>{title}</title>
+            </Helmet>
+            <main className="container-fluid">
+                <ChassNavbar username={undefined}></ChassNavbar>
+                <div className="px-4 py-5 my-5 text-center">
+                    <h1 className="display-5 fw-bold">{"Wizzard Chess"}</h1>
+                    <div className="col-lg-6 mx-auto">
+                        {!gameId && <CreateGame setGameId={setGameId} />}
+                        {gameId && <StreamGame setIsUsersTurn={setIsUsersTurn} gameId={gameId} magnet={magnet} />}
+                        {gameId && <MakeMove gameId={gameId} isUsersTurn={isUsersTurn} />}
+                        <BluetoothButton magnet={magnet} />
+                    </div>
+                </div>
+            </main>
 
-            {!gameId && <CreateGame setGameId={setGameId} />}
-            {gameId && <StreamGame setIsUsersTurn={setIsUsersTurn} gameId={gameId} magnet={magnet} />}
-            {gameId && <MakeMove gameId={gameId} isUsersTurn={isUsersTurn} />}
-            <BluetoothButton magnet={magnet} />
         </>
     )
 }
